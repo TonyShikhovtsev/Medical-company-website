8549e607811228e8ec53773051adb99e45deab73
TonyShikhovtsev <ssvsr2@gmail.com>
2024-02-16 02:03:32 +0500

    Добавление класса ууслуг, подготовка БД к загрузке данных

---
+++ medical_services/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from medical_services.models import Medical_service
+
+@admin.register(Medical_service)
+class Medical_serviceadmin(admin.ModelAdmin):
+    list_display = ('name', 'short_name',  'description', 'image', 'price')
+    list_filter = ('name', 'price')
---
+++ medical_services/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Medical_service(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование услуги')
+    short_name = models.CharField(max_length=50, verbose_name='Скоращенное наименование услуги')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    image = models.ImageField(verbose_name='изображение (превью)', upload_to='media/', **NULLABLE)
+    price = models.IntegerField(**NULLABLE, verbose_name='Стоимость')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
